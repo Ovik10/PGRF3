bd84a754693379708931e9d8c584ab47f4bc65a3
Milan Košťák <milankostak@seznam.cz>
2021-10-19 15:27:16 +0900

    základní ovládání kamery

--- src/main/Renderer.java
+++ src/main/Renderer.java
@@ -2,10 +2,13 @@ package main;
 
 import lwjglutils.*;
 import org.lwjgl.glfw.GLFWCursorPosCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.glfw.GLFWMouseButtonCallback;
 import transforms.*;
 
 import java.io.IOException;
 
+import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL20.*;
 
@@ -78,11 +81,61 @@ public class Renderer extends AbstractRenderer {
 
     @Override
     public GLFWCursorPosCallback getCursorCallback() {
-        return new GLFWCursorPosCallback() {
-            @Override
-            public void invoke(long window, double xpos, double ypos) {
-
-            }
-        };
+        return cursorPosCallback;
     }
+
+    @Override
+    public GLFWMouseButtonCallback getMouseCallback() {
+        return mouseButtonCallback;
+    }
+
+    @Override
+    public GLFWKeyCallback getKeyCallback() {
+        return keyCallback;
+    }
+
+    private double oldMx, oldMy;
+    private boolean mousePressed;
+
+    private final GLFWCursorPosCallback cursorPosCallback = new GLFWCursorPosCallback() {
+        @Override
+        public void invoke(long window, double x, double y) {
+            if (mousePressed) {
+                camera = camera.addAzimuth(Math.PI * (oldMx - x) / LwjglWindow.WIDTH);
+                camera = camera.addZenith(Math.PI * (oldMy - y) / LwjglWindow.HEIGHT);
+                oldMx = x;
+                oldMy = y;
+            }
+        }
+    };
+
+    private final GLFWMouseButtonCallback mouseButtonCallback = new GLFWMouseButtonCallback() {
+        @Override
+        public void invoke(long window, int button, int action, int mods) {
+            if (button == GLFW_MOUSE_BUTTON_LEFT) {
+                double[] xPos = new double[1];
+                double[] yPos = new double[1];
+                glfwGetCursorPos(window, xPos, yPos);
+                oldMx = xPos[0];
+                oldMy = yPos[0];
+                mousePressed = (action == GLFW_PRESS);
+            }
+        }
+    };
+
+    private final GLFWKeyCallback keyCallback = new GLFWKeyCallback() {
+        @Override
+        public void invoke(long window, int key, int scancode, int action, int mods) {
+            if (action == GLFW_PRESS || action == GLFW_REPEAT) {
+                switch (key) {
+                    case GLFW_KEY_A -> camera = camera.left(0.1);
+                    case GLFW_KEY_D -> camera = camera.right(0.1);
+                    case GLFW_KEY_W -> camera = camera.forward(0.1);
+                    case GLFW_KEY_S -> camera = camera.backward(0.1);
+                    case GLFW_KEY_R -> camera = camera.up(0.1);
+                    case GLFW_KEY_F -> camera = camera.down(0.1);
+                }
+            }
+        }
+    };
 }
